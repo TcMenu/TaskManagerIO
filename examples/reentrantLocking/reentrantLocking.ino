7b2319c64d56ce6bc3680592a4ff9d44cbc77a53
dave <dave@thecoderscorner.com>
2021-04-14 08:12:21 +0100

    #25 example updated

--- examples/reentrantLocking/reentrantLocking.ino
+++ examples/reentrantLocking/reentrantLocking.ino
@@ -2,16 +2,23 @@
 #include <TaskManagerIO.h>
 #include <ReentrantYieldingLock.h>
 
-// this is the global lock that can only be
+// this is the global lock that that we'll use to protect the variable below myVar.
 ReentrantYieldingLock myLock;
 volatile int myVar = 0;
 
+//
+// A simple log function for writing to serial
+//
 void log(const char* str, int val) {
     Serial.print(millis());
     Serial.print(' ');
     Serial.println(str);
 }
 
+//
+// here we define a nested function that is called from the task, it locks again. This test that the reentrant
+// functionality is working
+//
 void nestedFunction() {
     TaskMgrLock locker(myLock);
     log("in nested function", myVar);
@@ -20,18 +27,25 @@ void nestedFunction() {
 void setup() {
     Serial.begin(115200);
 
+    // start a task that locks the bus, calls a nested function and yields time back to task manager.
     taskManager.scheduleFixedRate(1000, [] {
         TaskMgrLock locker(myLock);
+        int myVarAtStart = myVar;
         log("start task function", myVar);
 
         // the nested function will lock again, which is fine because it's in the same task
         nestedFunction();
 
-        // now we release taskmanager to run other tasks, the will not be able to take our lock
+        // now we release task manager to run other tasks, the will not be able to take our lock
         taskManager.yieldForMicros(millisToMicros(500));
 
-        // now we have the context back lock again
+        // now we have the context back call the nested lock again
         nestedFunction();
+
+        if(myVar != myVarAtStart) {
+            log("ERROR in locking ", myVarAtStart);
+        }
+
         log("exit task function", myVar);
     });
 
